3d5b1c036492d20a12241e8a8e2a55f3beca6826
Guransh Dua <guransh.dua1@gmail.com>
2019-10-17 00:54:03 +0530

    Home Page Complete

--- index.php
+++ index.php
@@ -18,12 +18,116 @@
     </section>
     <section id="home2">
       <div style="text-align:center; padding-top:20px; padding-bottom:20px;">
-          <h2>Featured</h2>
+        <h3 class="pt-2 pb-2">Why CenPharma?</h3>
+        <p class="desc pb-2">
+          We provide the best in-class pharmaceutical services at a single place.
+        </p>
       </div>
-      <div class="container">
+      <div class="container mr-5 ml-5">
         <div class="row">
+          <div class="col4">
+            <div class="card white" style="justify-content:center; padding-top:0px;">
+              <div class="box">
+                <i class="fa fa-map-marker icon" style=""></i>
+                <h6 class="fw600" style="font-size:18px;">Order from any Pharmacy.</h6>
+                <p class="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col4">
+            <div class="card white" style="justify-content:center; padding-top:0px;">
+              <div class="box">
+                <i class="fa fa-clock-o icon" style=""></i>
+                <h6 class="fw600" style="font-size:18px;">Always available</h6>
+                <p class="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col4">
+            <div class="card white" style="justify-content:center; padding-top:0px;">
+              <div class="box">
+                <i class="fa fa-list-alt icon" style=""></i>
+                <h6 class="fw600" style="font-size:18px;">Trusted</h6>
+                <p class="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="home2">
+      <div style="text-align:center; padding-top:20px; padding-bottom:20px;">
+        <h3 class="pt-2 pb-2">Best Offers</h3>
+    </div>
+    <div class="container">
+      <div class="row mb-2" style="justify-content: center;">
+        <div class="sixcol">
+          <div class="card homecard zoom" style="justify-content: center;">
+            <div>
+              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+            </div>
+            <h6>Himalaya Tagara</h6>
+            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+            <h6 style="margin-top:10px">₹160</h6>
+          </div>
+        </div>
+        <div class="sixcol">
+          <div class="card homecard zoom" style="justify-content: center;">
+            <div>
+              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+            </div>
+            <h6>Himalaya Tagara</h6>
+            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+            <h6 style="margin-top:10px">₹160</h6>
+          </div>
+        </div>
+        <div class="sixcol">
+          <div class="card homecard zoom" style="justify-content: center;">
+            <div>
+              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+            </div>
+            <h6>Himalaya Tagara</h6>
+            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+            <h6 style="margin-top:10px">₹160</h6>
+          </div>
+        </div>
+        <div class="sixcol">
+          <div class="card homecard zoom" style="justify-content: center;">
+            <div>
+              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+            </div>
+            <h6>Himalaya Tagara</h6>
+            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+            <h6 style="margin-top:10px">₹160</h6>
+          </div>
+        </div>
+        <div class="sixcol">
+          <div class="card homecard zoom" style="justify-content: center;">
+            <div>
+              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+            </div>
+            <h6>Himalaya Tagara</h6>
+            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+            <h6 style="margin-top:10px">₹160</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+    <section id="home2">
+      <div style="text-align:center; padding-top:20px; padding-bottom:20px;">
+          <h3 class="pt-2 pb-2">Featured Categories</h3>
+      </div>
+      <div class="container ml-2 mr-2">
+        <div class="row mb-2">
           <div class="quarcol">
-            <div class="card zoom" style="justify-content: center;">
+            <div class="card homecard zoom" style="justify-content: center;">
               <div>
                 <img src="Images/Home/icons/allopathy.png" class="home-card-image ">
               </div>
@@ -31,7 +135,7 @@
             </div>
           </div>
           <div class="quarcol">
-            <div class="card zoom" style="justify-content: center;">
+            <div class="card homecard zoom" style="justify-content: center;">
               <div>
                 <img src="Images/Home/icons/homeopathy.png" class="home-card-image ">
               </div>
@@ -39,7 +143,7 @@
             </div>
           </div>
           <div class="quarcol">
-            <div class="card zoom" style="justify-content: center;">
+            <div class="card homecard zoom" style="justify-content: center;">
               <div>
                 <img src="Images/Home/icons/ayurveda.png" class="home-card-image ">
               </div>
@@ -47,7 +151,7 @@
             </div>
           </div>
           <div class="quarcol">
-            <div class="card zoom" style="justify-content: center;">
+            <div class="card homecard zoom" style="justify-content: center;">
               <div>
                 <img src="Images/Home/icons/wellness.png" class="home-card-image ">
               </div>
@@ -59,64 +163,50 @@
     </section>
     <section id="home2">
       <div style="text-align:center; padding-top:20px; padding-bottom:20px;">
-        <h2>Best Offers</h2>
-    </div>
-    <div class="container">
-      <div class="row" style="justify-content: center;">
-        <div class="sixcol">
-          <div class="card zoom" style="justify-content: center;">
-            <div>
-              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
-            </div>
-            <h6>Himalaya Tagara</h6>
-            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
-            <h6 style="margin-top:10px">₹160</h6>
-          </div>
-        </div>
-        <div class="sixcol">
-          <div class="card zoom" style="justify-content: center;">
-            <div>
-              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
-            </div>
-            <h6>Himalaya Tagara</h6>
-            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
-            <h6 style="margin-top:10px">₹160</h6>
-          </div>
-        </div>
-        <div class="sixcol">
-          <div class="card zoom" style="justify-content: center;">
-            <div>
-              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
-            </div>
-            <h6>Himalaya Tagara</h6>
-            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
-            <h6 style="margin-top:10px">₹160</h6>
-          </div>
-        </div>
-        <div class="sixcol">
-          <div class="card zoom" style="justify-content: center;">
-            <div>
-              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
-            </div>
-            <h6>Himalaya Tagara</h6>
-            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
-            <h6 style="margin-top:10px">₹160</h6>
-          </div>
-        </div>
-        <div class="sixcol">
-          <div class="card zoom" style="justify-content: center;">
-            <div>
-              <img src="Images/products/Himalaya/Tagara.png" class="home-prod-image ">
-            </div>
-            <h6>Himalaya Tagara</h6>
-            <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
-            <h6 style="margin-top:10px">₹160</h6>
-          </div>
-        </div>
-        
-        
+        <h3 class="pt-2 pb-2">Team CenPharma</h3>
+        <p class="desc pb-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
-    </div>
+      <div class="container mr-5 ml-5">
+        <div class="row" style="justify-content:center;">
+          <div class="col4">
+            <div class="card white" style="justify-content:center; padding-top:0px;">
+              <div class="box">
+                <img class="round-circle-img-home"src="Images/Home/IMG_6723_1.jpg" />
+                <h4 class="fw600" style="margin-bottom:5px;">Guransh Dua</h4>
+                <p class="title" style="margin-bottom:15px;">Developer</p>
+                <p class="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col4">
+            <div class="card white" style="justify-content:center; padding-top:0px;">
+              <div class="box">
+                <img class="round-circle-img-home"src="Images/Home/IMG_0115<div class="col4">
+                  <div class="card white" style="justify-content:center; padding-top:0px;">
+                    <div class="box">
+                      <img class="round-circle-img-home"src="Images/Home/IMG_6723_1.jpg" />
+                      <h4 class="fw600" style="margin-bottom:5px;">Guransh Dua</h4>
+                      <p class="title" style="margin-bottom:15px;">Developer</p>
+                      <p class="desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      </p>
+                    </div>
+                  </div>
+                <h4 class="fw600" style="margin-bottom:5px;">Rhythm Goyal</h4>
+                <p class="title" style="margin-bottom:15px;">Developer</p>
+                <p class="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </section>
     <?php
     require("./partials/footerhome.php");
--- medicine/index.php
+++ medicine/index.php
@@ -12,14 +12,39 @@
             <div class="row">
               <div class="halfcol">
                 <input type="search" class="myform" placeholder="Search"/>
-              </div>  
+              </div>
                 <button class="mybtn btn-blue">Search Medicines!</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-            </div>  
+            </div>
         </div>
       </nav>
-      <div class="container">
+      <div class="container" style="margin-top:2rem;">
         <div class="row">
-          <div class="quarcol">
+          <div class="sixcol">
+            <h5 class="fw600">Filters:</h5>
+            <input type="checkbox"  /> Allopathy<br />
+            <input type="checkbox" /> Homeopathy<br />
+            <input type="checkbox" /> Wellness<br />
+            <input type="checkbox" /> Ayurveda<br />
+          </div>
+          <div class="col" style="border-left:1px solid rgb(40,40,40,0.5);">
+            <div class="container" style="padding:0rem 3rem;">
+              <h2 style="text-align:center;">Medicines</h2>
+              <div class="row">
+                <div class="quarcol">
+                  <div class="card">
+                    <div>
+                      <img src="../Images/products/Himalaya/Tagara.png" class="home-prod-image ">
+                    </div>
+                    <h6 style="margin-bottom:0px;">Himalaya Tagara</h6>
+                    <p style="font-size:9px; margin-bottom: 0.5rem; color:#555;">
+                      Ayurvedic 
+                    </p>
+                    <p class="prod-desc">Bottle of 60 Tablets<br>MRP:₹152 (20% Off)<br></p>
+                    <h6 style="margin-top:10px">₹160</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
